feedbdaf5d5880cdb48a45a1080607125c7bdb50
Nick McCurdy <nick@nickmccurdy.com>
2022-06-30 05:30:20 -0400

    Initial implementation

---
+++ index.ts
@@ -0,0 +1,40 @@
+import { Octokit } from "octokit"
+
+const octokit = new Octokit({
+  auth: "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",
+})
+
+type Project = Awaited<ReturnType<typeof octokit.rest.projects.get>>
+
+async function getRepositoryProjects(
+  owner: string,
+): Promise<Project["data"][]> {
+  const repos = (await octokit.rest.repos.listForOrg({ org: owner })).data
+
+  return (
+    await Promise.all(
+      repos
+        .filter((repo) => repo.has_projects)
+        .map(async ({ name: repo }) => {
+          const { data } = await octokit.rest.projects.listForRepo({
+            owner,
+            repo,
+          })
+          return data
+        }),
+    )
+  ).flat()
+}
+
+async function getProjects(org: string): Promise<Project["data"][]> {
+  const projects: Project["data"][] = (
+    await octokit.rest.projects.listForOrg({ org })
+  ).data
+
+  return [...projects, ...(await getRepositoryProjects(org))]
+}
+
+;(async () => {
+  const projects = await getProjects("testing-library")
+  console.log(projects.map((project) => project.name))
+})()
