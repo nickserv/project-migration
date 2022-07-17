b78fa066873af98b6843d8b98297094e90ef27c9
Nick McCurdy <nick@nickmccurdy.com>
2022-07-17 08:10:21 -0400

    Use OAuth App device flow

--- README.md
+++ README.md
@@ -5,9 +5,7 @@ A tool for finding old GitHub Projects to use with [Project Migration](https://d
 ## Usage
 
 1. `npm install`
-2. Copy `.env.example` to `.env`
-3. [Generate a new personal access token](https://github.com/settings/tokens/new?description=Project+Migration&scopes=read:project,repo), copy it, and paste it into `.env`
-4. `npm start`
+2. `npm start`
 
 ## License
 [Table Icon](https://primer.style/octicons/table-24) from [Octicons](https://primer.style/octicons/) [[MIT License © 2022 GitHub Inc.](https://github.com/primer/octicons/blob/main/LICENSE)]
--- index.ts
+++ index.ts
@@ -1,66 +1,97 @@
-import { Octokit } from "octokit"
+import { readFile, writeFile } from "fs/promises"
+import { OAuthApp, Octokit } from "octokit"
 import { OctokitResponse } from "@octokit/types"
-import "dotenv/config"
-
-const octokit = new Octokit({ auth: process.env.GITHUB_TOKEN })
+import { inspect } from "util"
 
 type Response = Promise<OctokitResponse<unknown>>
 type ResponseData<T extends () => Response> = Awaited<ReturnType<T>>["data"]
-type Project = ResponseData<typeof octokit.rest.projects.get>
-type Repo = ResponseData<typeof octokit.rest.repos.get>
-
-async function listForRepositories(
-  owner: string,
-  repos: Repo[],
-): Promise<Project[]> {
-  return (
-    await Promise.all(
-      repos
-        .filter((repo) => repo.has_projects && !repo.archived)
-        .map(
-          async ({ name: repo }) =>
-            await octokit.paginate(octokit.rest.projects.listForRepo, {
-              owner,
-              repo,
-            }),
-        ),
-    )
-  ).flat()
-}
-
-async function listForUser(username: string): Promise<Project[]> {
-  return [
-    ...(await octokit.paginate(octokit.rest.projects.listForUser, {
-      username,
-    })),
-    ...(await listForRepositories(
-      username,
-      (await octokit.paginate(octokit.rest.repos.listForUser, {
-        username,
-      })) as Repo[],
-    )),
-  ]
-}
-
-async function listForOrg(org: string): Promise<Project[]> {
-  return [
-    ...(await octokit.paginate(octokit.rest.projects.listForOrg, {
-      org,
-    })),
-    ...(await listForRepositories(
-      org,
-      (await octokit.paginate(octokit.rest.repos.listForOrg, {
-        org,
-      })) as Repo[],
-    )),
-  ]
-}
+type Project = ResponseData<Octokit["rest"]["projects"]["get"]>
+type Repo = ResponseData<Octokit["rest"]["repos"]["get"]>
 
 function logProjects(projects: Project[]) {
   for (const project of projects) console.log(project.html_url)
 }
 
+async function getAuthToken(): Promise<string> {
+  try {
+    return await readFile(".cache", { encoding: "utf8" })
+  } catch (e) {
+    if (e && e instanceof Error) {
+      inspect(e.name)
+    }
+
+    // @ts-expect-error Device flow only requires clientId
+    const app = new OAuthApp({ clientId: "6792d6c6c07b364a8221" })
+
+    const authToken = new Promise<string>((resolve) => {
+      app.on("token", async ({ token }) => resolve(token))
+    })
+
+    await app.createToken({
+      scopes: ["read:project", "repo"],
+      onVerification(verification) {
+        console.log(
+          `Your code is ${verification.user_code}. Enter it at ${verification.verification_uri}`,
+        )
+      },
+    })
+
+    await writeFile(".cache", await authToken)
+
+    return authToken
+  }
+}
+
 ;(async () => {
+  const octokit = new Octokit({ auth: await getAuthToken() })
+
+  async function listForRepositories(
+    owner: string,
+    repos: Repo[],
+  ): Promise<Project[]> {
+    return (
+      await Promise.all(
+        repos
+          .filter((repo) => repo.has_projects && !repo.archived)
+          .map(
+            async ({ name: repo }) =>
+              await octokit.paginate(octokit.rest.projects.listForRepo, {
+                owner,
+                repo,
+              }),
+          ),
+      )
+    ).flat()
+  }
+
+  async function listForUser(username: string): Promise<Project[]> {
+    return [
+      ...(await octokit.paginate(octokit.rest.projects.listForUser, {
+        username,
+      })),
+      ...(await listForRepositories(
+        username,
+        (await octokit.paginate(octokit.rest.repos.listForUser, {
+          username,
+        })) as Repo[],
+      )),
+    ]
+  }
+
+  async function listForOrg(org: string): Promise<Project[]> {
+    return [
+      ...(await octokit.paginate(octokit.rest.projects.listForOrg, {
+        org,
+      })),
+      ...(await listForRepositories(
+        org,
+        (await octokit.paginate(octokit.rest.repos.listForOrg, {
+          org,
+        })) as Repo[],
+      )),
+    ]
+  }
+
   const { login } = (await octokit.rest.users.getAuthenticated()).data
   logProjects(await listForUser(login))
   const orgs = await octokit.paginate(
